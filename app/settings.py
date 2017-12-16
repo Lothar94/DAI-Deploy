69e753565fa120e537969b44bf5f1bcdf2091271
Lothar Soto Palma <lsp.lot94@gmail.com>
2017-12-16 20:34:07 +0100

    change static

--- app/settings.py
+++ app/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 REGISTRATION_OPEN = True                # If True, users can register
