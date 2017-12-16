1b72d7dcb391bba5f4c7927cf36dc9540b5e4268
Lothar Soto Palma <lsp.lot94@gmail.com>
2017-12-16 20:29:26 +0100

    db

--- restaurantes/models.py
+++ restaurantes/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from pymongo import MongoClient
 
-client = MongoClient()
+client = MongoClient("mongodb://rafa:daipass123@cluster0-shard-00-00-1ivyw.mongodb.net:27017,cluster0-shard-00-01-1ivyw.mongodb.net:27017,cluster0-shard-00-02-1ivyw.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin")
 db = client.test
 restaurants = db.restaurants
 
