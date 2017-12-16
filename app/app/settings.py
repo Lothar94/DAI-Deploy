3c52fe27226306e9d8500043b8315094c6b38272
Lothar Soto Palma <lsp.lot94@gmail.com>
2017-12-16 19:13:29 +0100

    deploy

--- app/app/settings.py
+++ app/app/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'kzunwih8vl%&6apk%hn%=$-rvrrbs#gi4$rlw*t6k*n5$n1q=v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
